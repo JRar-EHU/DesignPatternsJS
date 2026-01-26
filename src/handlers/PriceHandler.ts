bcd23701ee152498726de7b0261a34b01f52e788
Dmitriy Yartsev <iartsev.dmitrii@student.ehu.lt>
2026-01-26 20:14:55 +0300

    feat: lab 4 initial commit

--- src/handlers/PriceHandler.ts
+++
@@ -1,9 +0,0 @@
-import OrderHandler from './OrderHandler';
-import { IOrder } from '../order/IOrder';
-
-export default class PriceHandler extends OrderHandler {
-  handle(order: IOrder): void {
-    console.log('Total price:', order.getPrice());
-    super.handle(order);
-  }
-}
--- src/order/IOrder.ts
+++
@@ -1,8 +0,0 @@
-export interface IOrder {
-  getPizzaName(): string;
-  getPrice(): number;
-  getStock(): number;
-  getOptions(): string[];
-  setStatus(status: string): void;
-  getStatus(): string;
-}
